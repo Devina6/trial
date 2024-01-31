743ecf309860788d397d401e468e125edc386ad4
Devina <work.devina@gmail.com>
2024-01-31 21:23:08 +0530

    getting started

---
+++ trial-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div className="App">
+      <h2>Lets get started!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ trial-project/src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+function ExpenseItem() {
+    return (
+      <div>
+        <body>
+          <header>
+            <h1>ExpenseItems</h1>
+          </header>
+          <h2>
+            <pre>Food Rs 10</pre>
+          </h2>
+          <h2>
+            <pre>Petrol Rs 100</pre>
+          </h2>
+          <h2>
+            <pre>Movies Rs 200</pre>
+          </h2>
+        </body>
+      </div>
+    );
+  }
+  export default ExpenseItem;
